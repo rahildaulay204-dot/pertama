0fb4e457684f09d0f578ae3e4602b6032ff858de
rahildaulay204-dot <rahildaulay204@gmail.com>
2026-09-15 17:30:50 +0700

    Add files via upload

---
+++ fungsi.py
@@ -0,0 +1,8 @@
+data_1 = "hello world"
+id_data_1 = id(data_1)
+
+print("data_1:", data_1)
+# output data_1: hello world
+
+print("id_data_1:", id_data_1)
+# output id_data_1: 19441xxxxxxxx
---
+++ in.py
@@ -0,0 +1,24 @@
+sample_list = [2, 3, 4]
+is_3_exists = 3 in sample_list
+print(is_3_exists)
+# output True
+
+sample_tuple = ("hello", "python")
+is_hello_exists = "hello" in sample_tuple
+print(is_hello_exists)
+# output True
+
+sample_dict = { "nama": "noval", "age": 12 }
+is_key_nama_exists = "nama" in sample_dict
+print(is_key_nama_exists)
+# output True
+
+sample_set = { "sesuk", "preiiii" }
+is_prei = "preiiii" in sample_set
+print(is_prei)
+# output True
+
+text = 'hello world'
+is_substring_exists = 'orl' in text
+print(is_substring_exists)
+# output True
